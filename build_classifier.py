5c85055bd62ad1cdcdcf394eb34d5f0728aab2d4
Andy Zhang <signupforandy@gmail.com>
2015-12-22 16:51:23 -0800

    Move categories into txtfile and clean up semantics

--- build_classifier.py
+++ build_classifier.py
@@ -8,33 +8,27 @@ from wiki_utils import get_and_save_text
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
-categories = [
-    "Rare_diseases",
-    "Infectious_diseases",
-    "Cancer",
-    "Congenital_disorders",
-    "Organs_(anatomy)",
-    "Machine_learning_algorithms",
-    "Medical_devices",
-]
+categories = [category.rstrip() for category in open("%s/categories.txt" % os.getcwd())]
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--data", help="dir to save or load existing X.txt, y.txt", default=os.getcwd())
-parser.add_argument("-m", "--model", help="path to save model pipeline", default="%s/model.pkl" % os.getcwd())
+parser.add_argument("-t", "--trainer", help="path to save trainer", default="%s/trainer.pkl" % os.getcwd())
 args = parser.parse_args()
 
 logging.info("Collecting data for all categories")
 
 # Initialize and collect data
 X_text, y_text = [], []
+test_size = 0.2
 
-if os.path.isfile("%s/X.txt" % args.data) and os.path.isfile("%s/y.txt" % args.data):
+# Load X_train.txt and y_train.txt or fetch train and test sets
+if os.path.isfile("%s/X_train.txt" % args.data) and os.path.isfile("%s/y_train.txt" % args.data):
     logging.info("Found data in %s: " % args.data)
-    with open("%s/X.txt" % args.data) as X_file, open("%s/y.txt" % args.data) as y_file:
+    with open("%s/X_train.txt" % args.data) as X_file, open("%s/y_train.txt" % args.data) as y_file:
         X_text = X_file.readlines()
         y_text = y_file.readlines()
 else:
-    X_text, y_text = get_and_save_text(categories, args.data)
+    X_text, y_text = get_and_save_text(categories, args.data, test_size)
 
 logging.info("Collected %i examples across %i categories" % (len(X_text), len(categories)))
 
@@ -53,9 +47,7 @@ trained_models = [trainer.train(X_text, y_text) for trainer in trainers]
 best_trainer = max(trained_models, key=lambda m: m.score)
 logging.info("Best model was %s with %f score" % (best_trainer.model_family, best_trainer.score))
 
-print(best_trainer.model)
+with open(args.trainer, "w") as f:
+    f.write(pickle.dumps(best_trainer))
 
-with open(args.model, "w") as model_file:
-    model_file.write(pickle.dumps(best_trainer.model))
-
-logging.info("Pickled and saved model to %s" % args.model)
+logging.info("Pickled and saved trainer to %s" % args.trainer)
--- wiki_utils.py
+++ wiki_utils.py
@@ -3,6 +3,7 @@ import re
 
 import requests
 from gensim.corpora import wikicorpus
+from sklearn.cross_validation import train_test_split
 
 wiki_api_base = 'https://en.wikipedia.org/w/api.php?'
 
@@ -17,51 +18,61 @@ article_query = wiki_api_base + 'action=query&format=json&prop=revisions&rvprop=
 logging.getLogger("requests").setLevel(logging.WARNING)
 
 
-def get_and_save_text(categories, data_dir):
+def get_and_save_text(categories, data_dir, test_size=0.2):
     # Collect pairs of text, label
     x_text = []
     y_text = []
 
     # Get text examples for all categories
     # Write results to file
-    with open("%s/X.txt" % data_dir, "w") as X_file, open("%s/y.txt" % data_dir, "w") as y_file:
-        for category in categories:
-            continue_param = '-||'
+    for category in categories:
+        continue_param = '-||'
 
-            logging.info("Collecting data for category: %s" % category)
+        logging.info("Collecting data for category: %s" % category)
 
-            batch_count = 0
+        batch_count = 0
 
-            # Get data in batches per wiki api limit
-            while continue_param:
-                logging.debug("Processing batch: %i" % batch_count)
-                batch_count += 1
+        # Get data in batches per wiki api limit
+        while continue_param:
+            logging.debug("Processing batch: %i" % batch_count)
+            batch_count += 1
 
-                # Continue query and update continue_param
-                category_resp = requests.get(category_query % (category, continue_param)).json()
-                if category_resp.get('continue'):
-                    continue_param = category_resp['continue'].get('cmcontinue')
-                else:
-                    # Finished
-                    continue_param = None
+            # Continue query and update continue_param
+            category_resp = requests.get(category_query % (category, continue_param)).json()
+            if category_resp.get('continue'):
+                continue_param = category_resp['continue'].get('cmcontinue')
+            else:
+                # Finished
+                continue_param = None
 
-                # This will bomb out if query is empty, which is what we want
-                page_ids = [page['pageid'] for page in category_resp['query']['categorymembers']]
+            # This will bomb out if query is empty, which is what we want
+            page_ids = [page['pageid'] for page in category_resp['query']['categorymembers']]
 
-                # Get all article text in batch and append to scraped_text
-                article_resp = requests.get(article_query % "|".join(map(str, page_ids))).json()
-                articles = [page['revisions'][0]['*'] for _, page in article_resp['query']['pages'].iteritems()]
-                for text in articles:
-                    preprocessed_text = preprocess_wiki_text(text)
-                    x_text.append(preprocessed_text)
-                    y_text.append(category)
+            # Get all article text in batch and append to scraped_text
+            article_resp = requests.get(article_query % "|".join(map(str, page_ids))).json()
+            articles = [page['revisions'][0]['*'] for _, page in article_resp['query']['pages'].iteritems()]
+            for text in articles:
+                preprocessed_text = preprocess_wiki_text(text)
+                x_text.append(preprocessed_text + "\n")
+                y_text.append(category + "\n")
 
-                    X_file.write(preprocessed_text + "\n")
-                    y_file.write(category + "\n")
+    x_train, x_test, y_train, y_test = train_test_split(x_text, y_text, test_size=test_size, stratify=y_text)
 
-    logging.info("Saved data to %s as X.txt and y.txt" % data_dir)
+    with open("%s/X_train.txt" % data_dir, "w") as f:
+        f.writelines(x_train)
 
-    return x_text, y_text
+    with open("%s/X_test.txt" % data_dir, "w") as f:
+        f.writelines(x_test)
+
+    with open("%s/y_train.txt" % data_dir, "w") as f:
+        f.writelines(y_train)
+
+    with open("%s/y_test.txt" % data_dir, "w") as f:
+        f.writelines(y_test)
+
+    logging.info("Saved data to %s as X_train.txt, X_test.txt, y_train.txt, and y_test.txt" % data_dir)
+
+    return x_train, y_train
 
 
 def filter_additional_items(text):
